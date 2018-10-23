95e62bd282e5f5bc0a0643d65de96208fae7f11b
jamesrbedwell <jamesrbedwell@gmail.com>
2018-10-23 15:08:49 +1100

    Created Login page that redirects to a dashboard erb, created dashboard erb

--- main.rb
+++ main.rb
@@ -14,6 +14,25 @@ get '/' do
   erb :index
 end
 
+get '/dashboard/:id' do
+  erb :dashboard
+end
+
+post '/session' do
+  #authenticate
+  user = User.find_by(email: params[:email])
+  #if user exists and the password is correct, take them to dashboard page.
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect to("/dashboard/#{user.id}")
+  #redirect
+  else  
+    erb :login
+  end
+  
+end
+
+
 
 
 
