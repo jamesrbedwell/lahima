66be93aa3c7649ed080f7554daecb3e9e68dbfb5
jamesrbedwell <jamesrbedwell@gmail.com>
2018-10-23 16:14:47 +1100

    ENABLED SESSIONSgad, Created Helpers for logged in, dashboard only viewable if logged in, removed layout from Index

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
+require 'pry'
 
 require_relative 'db_config'
 require_relative 'models/user' 
@@ -10,27 +11,50 @@ require_relative 'models/worker'
 require_relative 'models/job' 
 require_relative 'models/project' 
 
+enable :sessions
+
+helpers do 
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+end
+
 get '/' do
-  erb :index
+  erb :index, layout: false
 end
 
-get '/dashboard/:id' do
-  @user = User.find(params[:id])
-  erb :dashboard
-end
-
+# SESSION ROUTES
 post '/session' do
   #authenticate
   user = User.find_by(email: params[:email])
   #if user exists and the password is correct, take them to dashboard page.
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
-    redirect to("/dashboard/#{user.id}")
+    redirect to("/dashboard")
   #redirect
   else  
-    erb :login
+    erb :index
   end
-  
+end
+
+delete '/session' do 
+  # destory session
+  session[:user_id] = nil
+  # redirect
+  redirect to('/')
+end
+
+# DASHBOARD ROUTES
+get '/dashboard' do
+  redirect to '/' unless logged_in?
+  @user = User.find(current_user.id)
+  erb :dashboard
 end
 
 
