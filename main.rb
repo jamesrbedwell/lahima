43dd3c96c784b4058715f1e30b4d612683fb838f
jamesrbedwell <jamesrbedwell@gmail.com>
2018-10-23 17:12:19 +1100

    Created New Worker Form, and link from Dashboard

--- main.rb
+++ main.rb
@@ -58,14 +58,38 @@ get '/dashboard' do
 end
 
 # WORKER ROUTES
-get '/worker/:id' do
+get '/workers/new' do
+  redirect to '/' unless logged_in?
+  erb :new_worker
+end
+
+post '/workers' do
+  worker = Worker.new
+  worker.first_name = params[:first_name]
+  worker.last_name = params[:last_name]
+  worker.phone = params[:phone]
+  worker.email = params[:email]
+  worker.worker_type = params[:worker_type]
+  worker.pay_rate = params[:pay_rate]
+  worker.address1 = params[:address1]
+  worker.city = params[:city]
+  worker.state = params[:state]
+  worker.country = params[:country]
+  worker.post_code = params[:psot_code]
+  worker.user_id = current_user.id
+  worker.save
+
+  redirect to("/workers/#{worker.id}")
+end
+
+get '/workers/:id' do
   redirect to '/' unless logged_in?
   @worker = Worker.find(params[:id])
   erb :worker
 end
 
 # CLIENT ROUTES
-get '/client/:id' do
+get '/clients/:id' do
   redirect to '/' unless logged_in?
   @client = Client.find(params[:id])
   erb :client
@@ -73,7 +97,7 @@ end
 
 
 #CLIENT CONTACT ROUTES
-get '/client_contact/:id' do
+get '/client_contacts/:id' do
   redirect to '/' unless logged_in?
   @client_contact = ClientContact.find(params[:id])
   erb :client_contact
